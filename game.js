daa4de2933f924c98d8c766be458a5ab641ccfc3
KrispWaffle <ijwu345@gmail.com>
2024-03-27 21:26:19 -0700

    almost done tbh
    just need a couple more things and should be done. Just been busy

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
 const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+let choices = ["rock", "paper", "scissors"]
+let userChoice; 
+
+function compChoice(choice){
+    const compChoose = Math.floor(Math.random()*choices.length)
+    
+    const result = choice[compChoose]
+    console.log(result)
+    return result;
+
+}
+
+
+const comp = compChoice(choices)
+rock.addEventListener("click", function() {
+    userChoice = "rock";
+    console.log(userChoice);
+});
+paper.addEventListener("click", function() {
+    userChoice = "paper";
+    console.log(userChoice);
+});
+scissors.addEventListener("click", function() {
+    userChoice = "scissors";
+    console.log(userChoice);
+});
